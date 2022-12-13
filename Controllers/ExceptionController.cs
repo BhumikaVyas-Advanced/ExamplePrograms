6dae7680108c3a3ae7716bf83cd0bd923c6a63d2
Bhumika Vyas <Bhumika.Vyas@oneadvanced.com>
2022-12-13 15:33:47 +0530

    Add project files.

---
+++ Controllers/ExceptionController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLog;
+using NLog.Fluent;
+using NLog.Web;
+
+namespace ExceptionHandling.Controllers
+{
+    public class ExceptionController : Controller
+    {
+        private readonly Logger logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+
+        public IActionResult Index()
+        {
+            ViewData["counter"] = 0;
+            return View(ViewData);
+        }
+
+        
+        public IActionResult ExceptionCounter(int counter)
+        {
+            try
+            {
+                if (counter < 10)
+                {
+                    ViewData["counter"] = ++counter;
+                    return View("Index", ViewData);
+                }
+                else
+                {
+                    logger.Error("Exceeded the count limit");
+
+                    throw new InvalidDataException("You have exceeded the count limit");
+                    
+                }
+            }
+            catch(Exception ex)
+            {
+                logger.Error(ex);
+                return View();
+                throw;
+            }
+
+         
+        }
+    }
+}
